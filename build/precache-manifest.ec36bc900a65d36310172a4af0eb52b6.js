7e7eb01eb751141bdbac3a576358f7298579ec97
Vik Williamson <williamson.vik@gmail.com>
2020-03-16 17:41:17 -0400

    Initial application files
    Adding files manually due to incorrect repo initialzation

---
+++ build/precache-manifest.ec36bc900a65d36310172a4af0eb52b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953baba00d237e35fe6dc195652ee2c0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fcf3e0298fd91d8c0970",
+    "url": "/monsters-rolodex/static/css/main.3a6e170a.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf3e0298fd91d8c0970",
+    "url": "/monsters-rolodex/static/js/main.7ae8630f.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
